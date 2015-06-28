05c4089c6000748d5a16464b2714ccc8d66c7845
Conor Gilmer <conor.gilmer@gmail.com>
2015-06-28 22:30:26 +0100

    Java Read log

---
+++ java/ReadLogJava/src/ReadLogJava.java
@@ -0,0 +1,55 @@
+/*
+ * Read Java MySql database of Radio Log files
+ */
+import java.sql.*;
+
+public class ReadLogJava {
+
+	public static void main(String[] args) {
+		System.out.println("Read Log Book Entries");
+		
+		try
+	    {
+	      // create our mysql database connection
+	      String myDriver = "org.gjt.mm.mysql.Driver";
+	      String myUrl = "jdbc:mysql://localhost/opendata";
+	      Class.forName(myDriver);
+	      Connection conn = DriverManager.getConnection(myUrl, "root", "");
+	      System.out.println("Connected to DB.\n");
+	  
+	      // our SQL SELECT query.
+	      // if you only need a few columns, specify them by name instead of using "*"
+	      String query = "SELECT * FROM radiolog";
+
+	      // create the java statement
+	      Statement st = conn.createStatement();
+	  
+	      // execute the query, and get a java resultset
+	      ResultSet rs = st.executeQuery(query);
+	  
+	      // iterate through the java resultset
+	      while (rs.next())
+	      {
+	        int id = rs.getInt("id");
+	        String frequency = rs.getString("frequency");
+	        String name = rs.getString("name");
+	        String band = rs.getString("band");
+	        String type = rs.getString("type");
+	        String unit = "kHz";
+	        String times = rs.getString("times");
+
+	  
+	        // print the results
+	        System.out.format("%s \t %s%s \t%s %s\t %s \t%s\n", id, frequency, unit,band, type, name, times);
+	      }
+	      st.close();
+	    }
+	    catch (Exception e)
+	    {
+	      System.err.println("Got an exception! ");
+	      System.err.println(e.getMessage());
+	    }
+
+	}
+
+}
